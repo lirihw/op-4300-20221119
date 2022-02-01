7a9b94747caa54e8e433f696c721e408a9da7d49
lirihw <97090257+lirihw@users.noreply.github.com>
2022-02-02 06:27:47 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,7 +9,7 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
-#sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5./g' openwrt/package/lean/default-settings/files/zzz-default-settings
+sed -i '$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5./g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with luci-theme-argon-dark
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon-dark/g' openwrt/feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-dark/g' openwrt/feeds/luci/collections/luci/Makefile
