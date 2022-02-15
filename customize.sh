20d9bbfb10b7d6b32c33d996cb1e82027e1eb01d
lirihw <97090257+lirihw@users.noreply.github.com>
2022-02-15 09:04:00 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -12,5 +12,5 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 sed -i '$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF./$1$wEehtjxj$YBu4quNfVUjzfv8p/PBo5./g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with luci-theme-argon-dark
-#sed -i 's/bootstrap/argon-dark/g' openwrt/feeds/luci/collections/luci/Makefile
-#sed -i 's/luci-theme-bootstrap/luci-theme-argon-dark/g' openwrt/feeds/luci/collections/luci/Makefile
+sed -i 's/bootstrap/argon-dark/g' openwrt/feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon-dark/g' openwrt/feeds/luci/collections/luci/Makefile
